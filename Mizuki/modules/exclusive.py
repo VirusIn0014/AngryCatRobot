17ac19167c13c0cffcacd0619a3959580d1619c4
VirusIn0014 <81487024+VirusIn0014@users.noreply.github.com>
2021-08-10 14:08:38 +0530

    Add files via upload

---
+++ Mizuki/modules/exclusive.py
@@ -0,0 +1,18 @@
+__help__ = """
+*𝐔𝐑𝐋 𝐔𝐩𝐥𝐨𝐚𝐝*
+ ⦁ `/upload`*:* reply to a direct download link to upload it to telegram as files.
+
+*𝐅𝐢𝐥𝐞 𝐓𝐨 𝐋𝐢𝐧𝐤*
+ ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link.
+
+*𝐕𝐢𝐫𝐮𝐬 𝐒𝐜𝐚𝐧*
+ ⦁ `/scanit`*:* Scan a file for virus (MAX SIZE = 3MB)
+
+*𝐌𝐞𝐦𝐞𝐬*
+ ⦁ `/memes`*:* get very interesting and funny memes randomly.
+
+*𝐓𝐨𝐫𝐫𝐞𝐧𝐭*
+ ⦁ `/torrent <query>`*:* search available torrents for your query.
+"""
+
+__mod_name__ = "Exclusive"
